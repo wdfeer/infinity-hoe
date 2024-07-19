dc000bdd381dd9af8e52f11f626930982774c44d
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-19 11:02:29 +0200

    Nerf GrowthAcceleration

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/GrowthAcceleration.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/GrowthAcceleration.kt
@@ -52,16 +52,16 @@ class GrowthAcceleration : HoeEnchantment(Rarity.UNCOMMON) {
                 block.grow(world, player.random, pos, state)
             }
 
-            player.blockPos.getAdjacent(level * 2)
+            player.blockPos.getAdjacent(level + 1)
                 .filter(::isApplicable)
-                .randoms(pow(level, 3))
+                .randoms(pow(level, 2) + 1)
                 .forEach(::apply)
         }
     }
 
     override fun getPath(): String = "growth_acceleration"
 
-    override fun getMinPower(level: Int): Int = 10 + level * 3
+    override fun getMinPower(level: Int): Int = 8 + level * 5
 
     override fun getMaxPower(level: Int): Int = 16 + level * 5
 
