d3bc7d5f4e0b620e6d6b17779a40e815f9be585d
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-20 22:08:46 +0200

    Delete LootTableLoader

--- src/main/kotlin/org/wdfeer/infinity_hoe/InfinityHoe.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/InfinityHoe.kt
@@ -3,7 +3,6 @@ package org.wdfeer.infinity_hoe
 import net.fabricmc.api.ModInitializer
 import org.wdfeer.infinity_hoe.enchantment.ModEnchantments
 import org.wdfeer.infinity_hoe.event.CropBreakListener
-import org.wdfeer.infinity_hoe.loot_tables.LootTableLoader
 
 object InfinityHoe : ModInitializer {
 	const val MOD_ID: String = "infinity_hoe"
@@ -11,6 +10,5 @@ object InfinityHoe : ModInitializer {
 	override fun onInitialize() {
 		ModEnchantments.initialize()
 		CropBreakListener.initialize()
-		LootTableLoader.initialize()
 	}
 }
