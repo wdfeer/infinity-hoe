f53e1195f2886d028ff5f78bd059b8a91a72af5d
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-22 11:11:03 +0200

    Set Specialist min/max enchanting power

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/Specialist.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/Specialist.kt
@@ -16,6 +16,10 @@ import kotlin.random.Random
 class Specialist(private val path: String, private val crop: Block) : HoeEnchantment(Rarity.RARE) {
     override fun getPath(): String = path
 
+    override fun getMinPower(level: Int): Int = 10
+
+    override fun getMaxPower(level: Int): Int = 50
+
     override fun canAccept(other: Enchantment?): Boolean = other !is Specialist
 
     override fun onCropBroken(
