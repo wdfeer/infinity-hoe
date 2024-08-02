6d4963adca3b8514fa363699b560a43650552619
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-02 17:09:52 +0200

    Add CropExperience functionality

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/CropExperience.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/CropExperience.kt
@@ -1,7 +1,39 @@
 package org.wdfeer.infinity_hoe.enchantment.unique
 
+import net.minecraft.entity.ExperienceOrbEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Vec3d
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
+import org.wdfeer.infinity_hoe.util.getEnchantmentLevel
+import kotlin.math.floor
 
 class CropExperience : HoeEnchantment(Rarity.RARE) {
     override fun getPath(): String = "crop_experience"
+
+    override fun getMaxLevel(): Int = 5
+
+    override fun getMinPower(level: Int): Int = 18 + level * 3
+
+    override fun getMaxPower(level: Int): Int = 24 + level * 3
+
+    override fun onCropBroken(
+        world: ServerWorld,
+        player: ServerPlayerEntity,
+        hoe: ItemStack,
+        pos: BlockPos,
+        mature: Boolean
+    ) {
+        if (mature) ExperienceOrbEntity.spawn(
+            world,
+            pos.toCenterPos().let { Vec3d(it.x, floor(it.y), it.z) },
+            XP_PER_LEVEL * hoe.getEnchantmentLevel(this)
+        )
+    }
+
+    companion object {
+        const val XP_PER_LEVEL: Int = 1
+    }
 }
