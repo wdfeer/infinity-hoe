b97d76efb31dbd3bd6fc68652e8586acf73629ed
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-06 09:45:20 +0200

    Create client initializer

---
+++ src/main/kotlin/org/wdfeer/infinity_hoe/client/InfinityHoeClient.kt
@@ -0,0 +1,9 @@
+package org.wdfeer.infinity_hoe.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object InfinityHoeClient : ClientModInitializer {
+    override fun onInitializeClient() {
+        TODO("Not Implemented!")
+    }
+}
