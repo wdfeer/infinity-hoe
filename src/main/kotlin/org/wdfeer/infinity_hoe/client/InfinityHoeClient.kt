7c79b72cdd8fab6b226b5c9d99d15c71a241b31c
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-06 09:53:48 +0200

    Add "hoe" to BC client swingThruGrassBlacklist regex

--- src/main/kotlin/org/wdfeer/infinity_hoe/client/InfinityHoeClient.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/client/InfinityHoeClient.kt
@@ -1,9 +1,16 @@
 package org.wdfeer.infinity_hoe.client
 
+import net.bettercombat.client.BetterCombatClient
 import net.fabricmc.api.ClientModInitializer
 
 object InfinityHoeClient : ClientModInitializer {
     override fun onInitializeClient() {
-        TODO("Not Implemented!")
+        val currentPattern = BetterCombatClient.config.swingThruGrassBlacklist
+        val hoeRegex = "\\bhoe\\b" // Regex for "hoe"
+
+        if (!currentPattern.contains(hoeRegex)) {
+            val updatedPattern = "$currentPattern|$hoeRegex".trim('|')
+            BetterCombatClient.config.swingThruGrassBlacklist = updatedPattern
+        }
     }
 }
