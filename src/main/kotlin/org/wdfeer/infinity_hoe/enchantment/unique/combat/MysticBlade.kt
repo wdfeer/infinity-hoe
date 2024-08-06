60f44496bb1b61eda8500694b1bf21e5b4aa9fd3
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-06 12:25:57 +0200

    Add MysticBlade functionality

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/combat/MysticBlade.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/combat/MysticBlade.kt
@@ -1,11 +1,25 @@
 package org.wdfeer.infinity_hoe.enchantment.unique.combat
 
+import net.minecraft.enchantment.EnchantmentHelper
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.damage.DamageTypes
+import net.minecraft.item.ItemStack
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
+import org.wdfeer.infinity_hoe.event.listener.OnHitListener
+import org.wdfeer.infinity_hoe.util.DamageSourceHelper
 
-class MysticBlade : HoeEnchantment(Rarity.RARE) {
+class MysticBlade : HoeEnchantment(Rarity.RARE), OnHitListener {
     override fun getPowerRange(level: Int): IntRange = 10..50
 
     override fun getPath(): String = "mystic_blade"
 
-    // TODO: Add functionality
+    override fun onHit(hoe: ItemStack, target: LivingEntity, attacker: LivingEntity) {
+        target.hurtTime = 0
+        target.damage(DamageSourceHelper.create(target.world, DamageTypes.MAGIC, attacker), getEnchantAmount(hoe) * damagePerEnchant)
+    }
+
+    private val damagePerEnchant: Float = 1f
+
+    private fun getEnchantAmount(hoe: ItemStack): Int =
+        EnchantmentHelper.fromNbt(hoe.enchantments).values.sumOf { it + 1 }
 }
