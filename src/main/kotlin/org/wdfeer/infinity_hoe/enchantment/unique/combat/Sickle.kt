b60a0ebaac99164b26b3df2244ddf6c490ba46cb
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-07 11:47:12 +0200

    Buff damage with Sickle

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/combat/Sickle.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/combat/Sickle.kt
@@ -1,5 +1,9 @@
 package org.wdfeer.infinity_hoe.enchantment.unique.combat
 
+import net.minecraft.entity.EntityGroup
+
 class Sickle : BetterCombatEnchantment() {
     override fun getPath(): String = "sickle"
+
+    override fun getAttackDamage(level: Int, group: EntityGroup?): Float { return 0.5f }
 }
