31f0cd39482fc76643f1455fcf362f2d7dcf852e
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-10 10:19:33 +0200

    Add Golden and Netherite hoes to loot

--- src/main/kotlin/org/wdfeer/infinity_hoe/loot/Tables.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/loot/Tables.kt
@@ -12,8 +12,8 @@ import net.minecraft.loot.function.SetCountLootFunction
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider
 import net.minecraft.util.Identifier
 
-private fun randomlyEnchantedLoot(item: Item, count: Int, chance: Float) = LootPool.builder()
-    .with(getItemBuilder(item, count))
+private fun randomlyEnchantedLoot(item: Item, enchantCount: Int, chance: Float) = LootPool.builder()
+    .with(getItemBuilder(item, enchantCount))
     .conditionally(RandomChanceLootCondition.builder(chance))
 
 private fun getItemBuilder(item: Item, enchantments: Int): LeafEntry.Builder<*> {
@@ -26,14 +26,14 @@ private fun getItemBuilder(item: Item, enchantments: Int): LeafEntry.Builder<*> 
 }
 
 internal enum class Tables(val tables: List<Identifier>, val reward: LootPool.Builder) {
-    Starter(
+    Stone(
         listOf(
             LootTables.SPAWN_BONUS_CHEST,
             LootTables.VILLAGE_TOOLSMITH_CHEST,
         ),
         randomlyEnchantedLoot(Items.STONE_HOE, 1, 1f)
     ),
-    Easy(
+    IronCommon(
         listOf(
             LootTables.IGLOO_CHEST_CHEST,
             LootTables.SHIPWRECK_MAP_CHEST,
@@ -42,14 +42,12 @@ internal enum class Tables(val tables: List<Identifier>, val reward: LootPool.Bu
         ),
         randomlyEnchantedLoot(Items.IRON_HOE, 1, 0.4f)
     ),
-    Medium(
+    IronRare(
         listOf(
             LootTables.HERO_OF_THE_VILLAGE_FARMER_GIFT_GAMEPLAY,
             LootTables.ABANDONED_MINESHAFT_CHEST,
             LootTables.DESERT_PYRAMID_CHEST,
             LootTables.JUNGLE_TEMPLE_CHEST,
-            LootTables.BASTION_OTHER_CHEST,
-            LootTables.NETHER_BRIDGE_CHEST,
             LootTables.SHIPWRECK_TREASURE_CHEST,
             LootTables.BURIED_TREASURE_CHEST,
             LootTables.FISHING_TREASURE_GAMEPLAY,
@@ -57,12 +55,33 @@ internal enum class Tables(val tables: List<Identifier>, val reward: LootPool.Bu
         ),
         randomlyEnchantedLoot(Items.IRON_HOE, 2, 0.25f)
     ),
-    Hard(
+    GoldCommon(
+        listOf(
+            LootTables.RUINED_PORTAL_CHEST,
+            LootTables.BASTION_BRIDGE_CHEST,
+            LootTables.BASTION_OTHER_CHEST,
+            LootTables.NETHER_BRIDGE_CHEST,
+        ),
+        randomlyEnchantedLoot(Items.GOLDEN_HOE, 3, 0.2f)
+    ),
+    GoldRare(
         listOf(
             LootTables.BASTION_TREASURE_CHEST,
+        ),
+        randomlyEnchantedLoot(Items.GOLDEN_HOE, 5, 0.25f)
+    ),
+    Diamond(
+        listOf(
             LootTables.ANCIENT_CITY_CHEST,
             LootTables.END_CITY_TREASURE_CHEST,
         ),
         randomlyEnchantedLoot(Items.DIAMOND_HOE, 4, 0.2f)
-    )
+    ),
+    Netherite(
+        listOf(
+            LootTables.ANCIENT_CITY_CHEST,
+            LootTables.BASTION_TREASURE_CHEST,
+        ),
+        randomlyEnchantedLoot(Items.NETHERITE_HOE, 5, 0.03f)
+    ),
 }
