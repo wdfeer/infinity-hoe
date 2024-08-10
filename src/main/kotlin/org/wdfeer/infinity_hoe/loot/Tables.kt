6b4fba1e9e15cea75871612d7a22df2d12e1153e
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-10 09:57:47 +0200

    Expand loot tables

--- src/main/kotlin/org/wdfeer/infinity_hoe/loot/Tables.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/loot/Tables.kt
@@ -12,40 +12,6 @@ import net.minecraft.loot.function.SetCountLootFunction
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider
 import net.minecraft.util.Identifier
 
-internal enum class Tables(val tables: List<Identifier>, val reward: LootPool.Builder) {
-    Easy(
-        listOf(
-            LootTables.SPAWN_BONUS_CHEST,
-            LootTables.VILLAGE_DESERT_HOUSE_CHEST,
-            LootTables.VILLAGE_SNOWY_HOUSE_CHEST,
-            LootTables.VILLAGE_TAIGA_HOUSE_CHEST,
-            LootTables.VILLAGE_SAVANNA_HOUSE_CHEST,
-            LootTables.VILLAGE_PLAINS_CHEST,
-            LootTables.IGLOO_CHEST_CHEST
-        ),
-        randomlyEnchantedLoot(Items.STONE_HOE, 1, 0.4f)
-    ),
-    Medium(
-        listOf(
-            LootTables.HERO_OF_THE_VILLAGE_FARMER_GIFT_GAMEPLAY,
-            LootTables.ABANDONED_MINESHAFT_CHEST,
-            LootTables.DESERT_PYRAMID_CHEST,
-            LootTables.JUNGLE_TEMPLE_CHEST,
-            LootTables.BASTION_OTHER_CHEST,
-            LootTables.NETHER_BRIDGE_CHEST,
-        ),
-        randomlyEnchantedLoot(Items.IRON_HOE, 2, 0.25f)
-    ),
-    Hard(
-        listOf(
-            LootTables.BASTION_TREASURE_CHEST,
-            LootTables.ANCIENT_CITY_CHEST,
-            LootTables.END_CITY_TREASURE_CHEST
-        ),
-        randomlyEnchantedLoot(Items.DIAMOND_HOE, 4, 0.2f)
-    )
-}
-
 private fun randomlyEnchantedLoot(item: Item, count: Int, chance: Float) = LootPool.builder()
     .with(getItemBuilder(item, count))
     .conditionally(RandomChanceLootCondition.builder(chance))
@@ -57,4 +23,46 @@ private fun getItemBuilder(item: Item, enchantments: Int): LeafEntry.Builder<*> 
     repeat(enchantments) { builder.apply(EnchantRandomlyLootFunction.create()) }
 
     return builder
+}
+
+internal enum class Tables(val tables: List<Identifier>, val reward: LootPool.Builder) {
+    Starter(
+        listOf(
+            LootTables.SPAWN_BONUS_CHEST,
+            LootTables.VILLAGE_TOOLSMITH_CHEST,
+        ),
+        randomlyEnchantedLoot(Items.STONE_HOE, 1, 1f)
+    ),
+    Easy(
+        listOf(
+            LootTables.IGLOO_CHEST_CHEST,
+            LootTables.SHIPWRECK_MAP_CHEST,
+            LootTables.SHIPWRECK_SUPPLY_CHEST,
+            LootTables.VILLAGE_WEAPONSMITH_CHEST,
+        ),
+        randomlyEnchantedLoot(Items.IRON_HOE, 1, 0.4f)
+    ),
+    Medium(
+        listOf(
+            LootTables.HERO_OF_THE_VILLAGE_FARMER_GIFT_GAMEPLAY,
+            LootTables.ABANDONED_MINESHAFT_CHEST,
+            LootTables.DESERT_PYRAMID_CHEST,
+            LootTables.JUNGLE_TEMPLE_CHEST,
+            LootTables.BASTION_OTHER_CHEST,
+            LootTables.NETHER_BRIDGE_CHEST,
+            LootTables.SHIPWRECK_TREASURE_CHEST,
+            LootTables.BURIED_TREASURE_CHEST,
+            LootTables.FISHING_TREASURE_GAMEPLAY,
+            LootTables.WOODLAND_MANSION_CHEST
+        ),
+        randomlyEnchantedLoot(Items.IRON_HOE, 2, 0.25f)
+    ),
+    Hard(
+        listOf(
+            LootTables.BASTION_TREASURE_CHEST,
+            LootTables.ANCIENT_CITY_CHEST,
+            LootTables.END_CITY_TREASURE_CHEST,
+        ),
+        randomlyEnchantedLoot(Items.DIAMOND_HOE, 4, 0.2f)
+    )
 }
