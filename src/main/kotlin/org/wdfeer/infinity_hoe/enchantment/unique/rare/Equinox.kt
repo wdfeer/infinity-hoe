2b15f98f8eaeda7cd33acd7142da5a675a977800
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-13 17:14:55 +0200

    Rework Equinox

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/rare/Equinox.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/rare/Equinox.kt
@@ -7,32 +7,48 @@ import net.minecraft.entity.effect.StatusEffects
 import net.minecraft.item.ItemStack
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.math.BlockPos
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
 import org.wdfeer.infinity_hoe.enchantment.catalyze.CropCatalyzer
 import org.wdfeer.infinity_hoe.enchantment.unique.uncommon.GrowthAcceleration
+import org.wdfeer.infinity_hoe.event.listener.HarvestListener
 import org.wdfeer.infinity_hoe.event.listener.OnHitListener
+import org.wdfeer.infinity_hoe.util.TickDurationHelper.minutesToTicks
+import org.wdfeer.infinity_hoe.util.TickDurationHelper.secondsToTicks
 
-class Equinox : HoeEnchantment(Rarity.RARE), OnHitListener {
+class Equinox : HoeEnchantment(Rarity.RARE), OnHitListener, HarvestListener {
     companion object {
-        private const val STATUS_DURATION: Int = 100
+        private val MAX_DURATION: Int = minutesToTicks(15)
+        private val DURATION_INCREASE: Int = secondsToTicks(5)
     }
 
     override fun onHit(hoe: ItemStack, target: LivingEntity, attacker: LivingEntity) {
         if (attacker !is ServerPlayerEntity || attacker.world !is ServerWorld) return
 
-        val actions: List<() -> Unit> = listOf(
-            { target.addStatusEffect(StatusEffectInstance(StatusEffects.WEAKNESS, STATUS_DURATION), attacker) },
-            { attacker.addStatusEffect(StatusEffectInstance(StatusEffects.STRENGTH, STATUS_DURATION), attacker) },
-            { CropCatalyzer.trigger(attacker.world as ServerWorld, attacker, 1, hoe) }
-        )
+        CropCatalyzer.trigger(attacker.world as ServerWorld, attacker, 1, hoe)
+    }
 
-        actions.random()()
+    override fun onCropBroken(
+        world: ServerWorld,
+        player: ServerPlayerEntity,
+        hoe: ItemStack,
+        pos: BlockPos,
+        mature: Boolean
+    ) {
+        if (!mature) return
+
+        val strength = player.getStatusEffect(StatusEffects.STRENGTH)
+
+        if (strength == null)
+            player.addStatusEffect(StatusEffectInstance(StatusEffects.STRENGTH, DURATION_INCREASE), player)
+        else if (strength.amplifier == 0 && strength.duration < MAX_DURATION)
+            player.addStatusEffect(StatusEffectInstance(StatusEffects.STRENGTH, strength.duration + DURATION_INCREASE), player)
     }
 
 
     override fun getPath(): String = "equinox"
-    override fun getPowerRange(level: Int): IntRange = 18..50
+    override fun getPowerRange(level: Int): IntRange = 22..50
 
 
-    override fun canAccept(other: Enchantment?): Boolean = other !is GrowthAcceleration && other !is SoulSiphon
+    override fun canAccept(other: Enchantment?): Boolean = other !is GrowthAcceleration && other !is SoulSiphon && other !is StandUnited
 }
