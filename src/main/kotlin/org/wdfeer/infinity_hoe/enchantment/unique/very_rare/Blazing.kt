a5ad32c9969e39468acc18e28aa9fa80a09f5e4f
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-15 08:11:09 +0200

    Implement fireball-shooting function

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/very_rare/Blazing.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/very_rare/Blazing.kt
@@ -1,8 +1,10 @@
 package org.wdfeer.infinity_hoe.enchantment.unique.very_rare
 
+import net.minecraft.entity.projectile.FireballEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.math.Vec3d
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
 import org.wdfeer.infinity_hoe.event.listener.AirUseListener
 
@@ -12,6 +14,28 @@ class Blazing : HoeEnchantment(Rarity.VERY_RARE), AirUseListener {
     override fun getPath(): String = "blazing"
 
     override fun onUseInAir(world: ServerWorld, player: ServerPlayerEntity, stack: ItemStack) {
-        TODO("Not yet implemented")
+        fun createFireballEntity(): FireballEntity {
+            val fireball = FireballEntity(world, player, 0.0, 0.0, 0.0, 0)
+
+            val playerFacing = player.rotationClient
+            val playerPos = player.pos
+            val forwardVector = Vec3d(
+                -Math.sin(Math.toRadians(playerFacing.y.toDouble())),
+                0.0,
+                Math.cos(Math.toRadians(playerFacing.y.toDouble()))
+            )
+
+            fireball.updatePosition(
+                playerPos.x + forwardVector.x * 2.0,
+                playerPos.y + player.eyeY - 0.10000000149011612,
+                playerPos.z + forwardVector.z * 2.0
+            )
+
+            fireball.setVelocity(forwardVector.x * 3.0, 0.0, forwardVector.z * 3.0)
+
+            return fireball
+        }
+
+        world.spawnEntity(createFireballEntity())
     }
 }
