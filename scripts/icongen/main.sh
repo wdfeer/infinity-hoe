85ee7490ab09eb3b0c70e914d6b65fe9ad00c2f7
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-17 13:34:34 +0200

    Update magick script

--- scripts/icongen/main.sh
+++ scripts/icongen/main.sh
@@ -8,14 +8,14 @@ INPUT_IMAGE="src/main/resources/no_include/icon_base.png"
 OUTPUT_IMAGE="src/main/resources/assets/infinity_hoe/icon.png"
 
 # Font settings
-FONT_SIZE=8
+FONT_SIZE=64
 FONT_COLOR="white"
-PADDING=10
+FONT="Inter"
 
-# Use ImageMagick's convert command to add the text
 magick "$INPUT_IMAGE" \
     -gravity southeast \
     -fill "$FONT_COLOR" \
     -pointsize "$FONT_SIZE" \
-    -annotate +$PADDING+$PADDING "$TEXT" \
+    -font "$FONT" \
+    -annotate -5-5 "$TEXT" \
     "$OUTPUT_IMAGE"
