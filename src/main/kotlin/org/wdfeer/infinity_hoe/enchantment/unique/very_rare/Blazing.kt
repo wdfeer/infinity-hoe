a59630b91763878ee451520a2990b2d8eb9485b6
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-18 09:03:47 +0200

    Create abstract ChargeEnchantment class

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/very_rare/Blazing.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/very_rare/Blazing.kt
@@ -4,17 +4,13 @@ import net.minecraft.entity.projectile.FireballEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
-import net.minecraft.text.Style
-import net.minecraft.text.Text
 import net.minecraft.util.Formatting
-import net.minecraft.util.math.BlockPos
-import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
+import org.wdfeer.infinity_hoe.enchantment.charge.ChargeEnchantment
 import org.wdfeer.infinity_hoe.event.listener.AirUseListener
 import org.wdfeer.infinity_hoe.event.listener.AppendTooltipListener
 import org.wdfeer.infinity_hoe.event.listener.HarvestListener
-import org.wdfeer.infinity_hoe.extension.getEnchantmentLevel
 
-class Blazing : HoeEnchantment(Rarity.VERY_RARE), HarvestListener, AirUseListener, AppendTooltipListener {
+class Blazing : ChargeEnchantment(Rarity.VERY_RARE), HarvestListener, AirUseListener, AppendTooltipListener {
     override val maxLvl: Int
         get() = 2
 
@@ -22,38 +18,12 @@ class Blazing : HoeEnchantment(Rarity.VERY_RARE), HarvestListener, AirUseListene
 
     override fun getPath(): String = "blazing"
 
-    private val nbtKey = getPath() + "_charge"
-    private fun getMaxCharge(level: Int) = 50 * level
-
-    override fun onCropBroken(
-        world: ServerWorld,
-        player: ServerPlayerEntity,
-        hoe: ItemStack,
-        pos: BlockPos,
-        mature: Boolean
-    ) {
-        if (!mature) return
-
-        val nbt = hoe.orCreateNbt
-        val charge = nbt.getInt(nbtKey)
-        val level = hoe.getEnchantmentLevel(this)
-
-        if (nbt.contains(nbtKey) && charge < getMaxCharge(level)) {
-            nbt.putInt(nbtKey, charge + 1)
-        } else
-            nbt.putInt(nbtKey, 1)
-    }
-
-    override fun onUseInAir(world: ServerWorld, player: ServerPlayerEntity, hoe: ItemStack) {
-        val nbt = hoe.nbt ?: return
-        val charge = nbt.getInt(nbtKey)
-        if (!nbt.contains(nbtKey) || charge <= 0) return
-
+    override fun useCharge(world: ServerWorld, player: ServerPlayerEntity, hoe: ItemStack): Boolean {
         world.spawnEntity(createFireball(world, player))
 
         player.itemCooldownManager.set(hoe.item, 10)
 
-        nbt.putInt(nbtKey, charge - 1)
+        return true
     }
 
     private fun createFireball(
@@ -69,13 +39,5 @@ class Blazing : HoeEnchantment(Rarity.VERY_RARE), HarvestListener, AirUseListene
         return fireball
     }
 
-    override fun appendTooltip(stack: ItemStack, tooltip: MutableList<Text>) {
-        val nbt = stack.nbt
-        val charge = if (nbt?.contains(nbtKey) == true) nbt.getInt(nbtKey) else 0
-        val maxCharge = getMaxCharge(stack.getEnchantmentLevel(this))
-
-        tooltip.add(Text.translatable("tooltip.infinity_hoe.blazing.charge", charge, maxCharge).apply {
-            style = Style.EMPTY.withColor(if (charge > 0) Formatting.RED else Formatting.GRAY)
-        })
-    }
+    override fun getTooltipFormatting(): Formatting = Formatting.RED
 }
