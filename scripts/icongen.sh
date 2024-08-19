cbc3d441d54173c52e941c2b62a4b04745ee5ad8
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-19 08:11:21 +0200

    Split icongen script into 3

--- scripts/icongen.sh
+++ scripts/icongen.sh
@@ -4,19 +4,5 @@ set -a
 source "scripts/.env"
 set +a
 
-TEXT="$ENCHANTMENT_COUNT"
-
-INPUT_IMAGE="src/main/resources/no_include/icon_base.png"
-OUTPUT_IMAGE="src/main/resources/assets/infinity_hoe/icon.png"
-
-FONT_SIZE=64
-FONT_COLOR="white"
-FONT="Inter"
-
-magick "$INPUT_IMAGE" \
-    -gravity southeast \
-    -fill "$FONT_COLOR" \
-    -pointsize "$FONT_SIZE" \
-    -font "$FONT" \
-    -annotate -5-5 "$TEXT" \
-    "$OUTPUT_IMAGE"
+source "scripts/icongen/static.sh"
+source "scripts/icongen/animated.sh"
---
+++ scripts/icongen/static.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TEXT="$ENCHANTMENT_COUNT"
+
+INPUT_IMAGE="src/main/resources/no_include/icon_base.png"
+OUTPUT_IMAGE="src/main/resources/assets/infinity_hoe/icon.png"
+
+FONT_SIZE=64
+FONT_COLOR="white"
+FONT="Inter"
+
+magick "$INPUT_IMAGE" \
+    -gravity southeast \
+    -fill "$FONT_COLOR" \
+    -pointsize "$FONT_SIZE" \
+    -font "$FONT" \
+    -annotate -5-5 "$TEXT" \
+    "$OUTPUT_IMAGE"
