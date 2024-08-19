437cec682b2d26601eae9ad36b26944522f43f33
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-19 22:08:01 +0200

    Register a Rebound enchantment if simplyswords installed

---
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/uncommon/Rebound.kt
@@ -0,0 +1,7 @@
+package org.wdfeer.infinity_hoe.enchantment.unique.uncommon
+
+import org.wdfeer.infinity_hoe.enchantment.bc.BetterCombatEnchantment
+
+class Rebound : BetterCombatEnchantment() {
+    override fun getPath(): String = "rebound"
+}
