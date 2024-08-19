1ca3cf56a97417b891bd6c3a70ac92475f6a83ce
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-19 22:14:33 +0200

    Make Rebound reduce damage by 0.5

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/uncommon/Rebound.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/uncommon/Rebound.kt
@@ -1,7 +1,10 @@
 package org.wdfeer.infinity_hoe.enchantment.unique.uncommon
 
+import net.minecraft.entity.EntityGroup
 import org.wdfeer.infinity_hoe.enchantment.bc.BetterCombatEnchantment
 
 class Rebound : BetterCombatEnchantment() {
     override fun getPath(): String = "rebound"
+
+    override fun getAttackDamage(level: Int, group: EntityGroup?): Float = -0.5f
 }
