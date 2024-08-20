6fa40e2a12f6a5ef1e3c7fd0cb9a5c8cf45d75d0
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-20 08:11:10 +0200

    Implement DemeterWrath damage

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/common/DemeterWrath.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/common/DemeterWrath.kt
@@ -1,9 +1,32 @@
 package org.wdfeer.infinity_hoe.enchantment.unique.common
 
+import net.minecraft.block.Blocks
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.damage.DamageTypes
+import net.minecraft.item.ItemStack
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.stat.Stats
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
+import org.wdfeer.infinity_hoe.event.listener.PreAttackListener
+import org.wdfeer.infinity_hoe.extension.damage
+import kotlin.math.log10
 
-class DemeterWrath : HoeEnchantment(Rarity.COMMON) {
+class DemeterWrath : HoeEnchantment(Rarity.COMMON), PreAttackListener {
     override fun getPowerRange(level: Int): IntRange = 10 ..50
 
     override fun getPath(): String = "demeter_wrath"
+
+    private fun getCrops(player: ServerPlayerEntity): Int = player.statHandler.run { listOf(
+        getStat(Stats.MINED.getOrCreateStat(Blocks.BEETROOTS)),
+        getStat(Stats.MINED.getOrCreateStat(Blocks.CARROTS)),
+        getStat(Stats.MINED.getOrCreateStat(Blocks.POTATOES)),
+        getStat(Stats.MINED.getOrCreateStat(Blocks.WHEAT)),
+    ) }.sum()
+
+    private fun getDamage(player: ServerPlayerEntity): Float = log10(getCrops(player).toFloat())
+
+    override fun preAttack(player: ServerPlayerEntity, target: LivingEntity, hoe: ItemStack) {
+        target.hurtTime = 0
+        target.damage(DamageTypes.MAGIC, getDamage(player), player)
+    }
 }
