853b9cbc06255667223895c2d5ca4d1bcf91ccac
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-22 11:01:17 +0200

    Implement CalciumBurst

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/uncommon/CalciumBurst.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/uncommon/CalciumBurst.kt
@@ -1,18 +1,47 @@
 package org.wdfeer.infinity_hoe.enchantment.unique.uncommon
 
+import net.minecraft.block.CropBlock
+import net.minecraft.item.BoneMealItem
 import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
-import org.wdfeer.infinity_hoe.enchantment.chain.CalciumBurstAction
-import org.wdfeer.infinity_hoe.enchantment.chain.ChainEnchantment
-import org.wdfeer.infinity_hoe.event.listener.Useable
+import net.minecraft.util.math.BlockPos
+import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
+import org.wdfeer.infinity_hoe.event.listener.HarvestListener
+import org.wdfeer.infinity_hoe.extension.getAdjacent
+import org.wdfeer.infinity_hoe.extension.getEnchantmentLevel
+import org.wdfeer.infinity_hoe.extension.inventoryStacks
 
-class CalciumBurst : ChainEnchantment<CalciumBurstAction>(Rarity.UNCOMMON), Useable {
+class CalciumBurst : HoeEnchantment(Rarity.UNCOMMON), HarvestListener {
+    override val maxLvl: Int
+        get() = 2
     override fun getPowerRange(level: Int): IntRange = 13..50
 
     override fun getPath(): String = "calcium_burst"
 
-    override fun use(world: ServerWorld, player: ServerPlayerEntity, hoe: ItemStack) {
-        TODO("Not yet implemented")
+    override fun onCropBroken(
+        world: ServerWorld,
+        player: ServerPlayerEntity,
+        hoe: ItemStack,
+        pos: BlockPos,
+        mature: Boolean
+    ) {
+        if (mature) trigger(world, player, pos, hoe.getEnchantmentLevel(this))
+    }
+
+    private fun trigger(world: ServerWorld, player: ServerPlayerEntity, pos: BlockPos, level: Int) {
+        val boneMealStacks = player.inventoryStacks.filter { !it.isEmpty && it.item == Items.BONE_MEAL }
+
+        if (boneMealStacks.isEmpty()) return
+
+        pos.getAdjacent(1)
+            .filter { world.getBlockState(it).block is CropBlock }
+            .forEach { position ->
+            repeat(level) {
+                val stack = boneMealStacks.find { !it.isEmpty } ?: return@forEach
+                BoneMealItem.useOnFertilizable(stack, world, position)
+            }
+        }
     }
 }
--- src/main/kotlin/org/wdfeer/infinity_hoe/extension/PlayerInventory.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/extension/PlayerInventory.kt
@@ -2,6 +2,10 @@ package org.wdfeer.infinity_hoe.extension
 
 import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.item.ItemStack
+import net.minecraft.server.network.ServerPlayerEntity
+
+val ServerPlayerEntity.inventoryStacks: List<ItemStack>
+    get() = this.inventory.stacks
 
 val PlayerInventory.stacks: List<ItemStack>
     get() = List(this.size()) { this.getStack(it) }
