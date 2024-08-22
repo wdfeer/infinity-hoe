85bda368e201b69b3bd56feb6d1c026d8f3d958d
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-22 20:12:14 +0200

    Implement DemeterGrace healing

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/rare/DemeterGrace.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/rare/DemeterGrace.kt
@@ -4,22 +4,28 @@ import net.minecraft.enchantment.Enchantment
 import net.minecraft.server.network.ServerPlayerEntity
 import org.wdfeer.infinity_hoe.enchantment.parent.DemeterEnchantment
 import org.wdfeer.infinity_hoe.event.listener.PlayerDamageTaken
+import org.wdfeer.infinity_hoe.extension.roll
+import kotlin.random.Random
 
 class DemeterGrace : DemeterEnchantment(), PlayerDamageTaken {
     override fun getPath(): String = "demeter_grace"
 
     private fun getProcs(charge: Int): Float = charge / 200f
 
-    override fun getMaxCharge(level: Int): Int = 400
-    override fun getChargeDecrement(): Int = 200
+    override fun getMaxCharge(level: Int): Int = 1500
+    override fun getChargeDecrement(): Int = 75
     override fun chargeToString(charge: Int): String = "%.2f".format(getProcs(charge))
 
     override fun canAccept(other: Enchantment?): Boolean = super.canAccept(other) && other !is MysticBlade
 
     override fun postDamageTaken(player: ServerPlayerEntity, amount: Float) {
+        if (!Random.roll(amount * 0.4f + 0.2f)) return
+
+        if (player.maxHealth - player.health < 1f) return
+
         val hoe = player.handItems.find { getCharge(it) >= getChargeDecrement() } ?: return
 
-        // TODO: Implement healing
+        player.heal(amount / 4f + 1f)
 
         setCharge(hoe, getCharge(hoe) - getChargeDecrement())
     }
