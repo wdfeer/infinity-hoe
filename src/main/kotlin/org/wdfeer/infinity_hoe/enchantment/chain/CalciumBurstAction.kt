34a23da1876048141aa1e477ec110a3c1b1da49e
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-22 11:48:10 +0200

    Fix CalciumBurstAction not working

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/chain/CalciumBurstAction.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/chain/CalciumBurstAction.kt
@@ -10,7 +10,9 @@ import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.math.BlockPos
 import org.wdfeer.infinity_hoe.enchantment.EnchantmentLoader
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
+import org.wdfeer.infinity_hoe.extension.getEnchantmentLevel
 import org.wdfeer.infinity_hoe.extension.inventoryStacks
+import org.wdfeer.infinity_hoe.extension.randomRound
 
 class CalciumBurstAction(
     world: ServerWorld,
@@ -23,7 +25,7 @@ class CalciumBurstAction(
 
         if (boneMealStacks.isEmpty()) return
 
-        val count: Int = 1//(hoe.getEnchantmentLevel(getEnchantment()) * 0.3f + 0.5f).randomRound()
+        val count: Int = (hoe.getEnchantmentLevel(getEnchantment()) * 0.3f + 0.5f).randomRound()
         repeat(count) {
             val stack = boneMealStacks.find { !it.isEmpty } ?: return
             BoneMealItem.useOnFertilizable(stack, world, pos)
@@ -32,7 +34,7 @@ class CalciumBurstAction(
 
     override fun canDamageHoe(): Boolean = false
 
-    override fun isValidBlockState(state: BlockState): Boolean = (state.block as? CropBlock)?.isMature(state) == true
+    override fun isValidBlockState(state: BlockState): Boolean = (state.block as? CropBlock)?.isMature(state) == false
 
     override fun getEnchantment(): HoeEnchantment = EnchantmentLoader.calciumBurst
 }
