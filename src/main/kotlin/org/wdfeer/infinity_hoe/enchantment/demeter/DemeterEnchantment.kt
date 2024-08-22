48bba3ef6a38a8362687ec0f150d77ae4a24e0d4
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-22 19:43:28 +0200

    Make Demeters mutually incompatible

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/demeter/DemeterEnchantment.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/demeter/DemeterEnchantment.kt
@@ -3,12 +3,11 @@ package org.wdfeer.infinity_hoe.enchantment.demeter
 import net.minecraft.enchantment.Enchantment
 import net.minecraft.util.Formatting
 import org.wdfeer.infinity_hoe.enchantment.charge.HarvestChargeEnchantment
-import org.wdfeer.infinity_hoe.enchantment.charge.UsableHarvestChargeEnchantment
 
 abstract class DemeterEnchantment : HarvestChargeEnchantment(Rarity.RARE) {
     override fun getPowerRange(level: Int): IntRange = 10..50
 
     override fun getTooltipColor(): Formatting = Formatting.GREEN
 
-    override fun canAccept(other: Enchantment?): Boolean = other !is UsableHarvestChargeEnchantment
+    override fun canAccept(other: Enchantment?): Boolean = other !is DemeterEnchantment
 }
