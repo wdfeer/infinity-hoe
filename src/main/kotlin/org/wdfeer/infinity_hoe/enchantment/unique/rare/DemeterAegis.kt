6443690b088b2051458be98f66d02618ebae9fc6
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-08-23 10:29:22 +0200

    Rebalance DemeterAegis

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/rare/DemeterAegis.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/rare/DemeterAegis.kt
@@ -5,17 +5,21 @@ import net.minecraft.entity.effect.StatusEffects
 import net.minecraft.server.network.ServerPlayerEntity
 import org.wdfeer.infinity_hoe.enchantment.parent.DemeterEnchantment
 import org.wdfeer.infinity_hoe.event.listener.PlayerDamageTaken
+import org.wdfeer.infinity_hoe.extension.roll
+import kotlin.random.Random
 
 class DemeterAegis : DemeterEnchantment(), PlayerDamageTaken {
     override fun getPath(): String = "demeter_aegis"
 
-    private fun getNullifications(charge: Int): Float = charge / 200f
+    private fun getNullifications(charge: Int): Float = charge / getChargeDecrement().toFloat()
 
-    override fun getMaxCharge(level: Int): Int = 400
-    override fun getChargeDecrement(): Int = 200
+    override fun getMaxCharge(level: Int): Int = getChargeDecrement() * 6
+    override fun getChargeDecrement(): Int = 80
     override fun chargeToString(charge: Int): String = "%.2f".format(getNullifications(charge))
 
     override fun preDamageTaken(player: ServerPlayerEntity, amount: Float) {
+        if (!Random.roll(amount / 3f)) return
+
         val hoe = player.handItems.find { getCharge(it) >= getChargeDecrement() } ?: return
 
         if (player.statusEffects.any { it.effectType == StatusEffects.RESISTANCE && it.amplifier == 255 }) return
