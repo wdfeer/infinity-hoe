f4df9abe5321a7c27bce6d077bb0cb375393e1be
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-09-20 14:02:14 +0200

    Record Luna Dial previous player positions

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/very_rare/LunaDial.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/very_rare/LunaDial.kt
@@ -1,26 +1,57 @@
 package org.wdfeer.infinity_hoe.enchantment.unique.very_rare
 
+import net.minecraft.item.HoeItem
 import net.minecraft.item.ItemStack
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.Formatting
+import net.minecraft.util.math.Vec3d
+import org.joml.Vector3f
 import org.wdfeer.infinity_hoe.enchantment.parent.charge.UsableHarvestChargeEnchantment
 import org.wdfeer.infinity_hoe.event.listener.PlayerTicker
+import org.wdfeer.infinity_hoe.extension.hasEnchantment
+import org.wdfeer.infinity_hoe.extension.inventoryStacks
+import java.util.*
+import kotlin.collections.ArrayDeque
 
 class LunaDial : UsableHarvestChargeEnchantment(Rarity.VERY_RARE), PlayerTicker {
-    override fun canIteratePlayers(world: ServerWorld): Boolean {
-        TODO("Not yet implemented")
+    companion object {
+        const val POSITIONS_STORED = 200
+
+        // Vector3f instead of Vec3d to conserve RAM
+        val playerPositions: MutableMap<UUID, ArrayDeque<Vector3f>> = mutableMapOf()
     }
 
+    override fun canIteratePlayers(world: ServerWorld): Boolean = true
+
     override fun tickPlayer(world: ServerWorld, player: ServerPlayerEntity) {
-        TODO("Not yet implemented")
+        if (player.inventoryStacks.any { it.item is HoeItem && it.hasEnchantment(this)})
+            recordPosition(player)
+        else
+            playerPositions.remove(player.uuid)
+    }
+
+    private fun recordPosition(player: ServerPlayerEntity) {
+        val array = ArrayDeque<Vector3f>().let { playerPositions.put(player.uuid, it) ?: it }
+        array.addLast(player.pos.toVector3f())
+        if (array.size > POSITIONS_STORED)
+            array.removeFirst()
     }
 
     override fun useCharge(world: ServerWorld, player: ServerPlayerEntity, hoe: ItemStack): Boolean {
-        TODO("Not yet implemented")
+        // FIXME: right clicking doesn't have any effect
+        player.setPosition(playerPositions[player.uuid]?.first()?.run {
+            Vec3d(x.toDouble(), y.toDouble(), z.toDouble())
+        } ?: return false)
+
+        return true
     }
 
     override fun getMaxCharge(level: Int): Int = getChargeDecrement() * 16
     override fun getChargeDecrement(): Int = 50
+    override fun chargeToString(charge: Int): String = "%.2f".format(charge.toFloat() / getChargeDecrement())
+
+    override fun getTooltipColor(): Formatting = Formatting.DARK_AQUA
 
     override fun getPowerRange(level: Int): IntRange = 30..100
 
