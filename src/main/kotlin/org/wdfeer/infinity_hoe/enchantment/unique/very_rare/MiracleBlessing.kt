b69415e335cecce9b51e3588198bc5c563bdf55f
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-02-15 14:17:39 +0100

    Change MiracleBlessing from inventory tick to hold tick

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/very_rare/MiracleBlessing.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/very_rare/MiracleBlessing.kt
@@ -3,17 +3,18 @@ package org.wdfeer.infinity_hoe.enchantment.unique.very_rare
 import net.minecraft.enchantment.Enchantment
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.item.ItemStack
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
 import org.wdfeer.infinity_hoe.enchantment.unique.rare.AnimalBlessing
 import org.wdfeer.infinity_hoe.enchantment.unique.uncommon.GrowthAcceleration
-import org.wdfeer.infinity_hoe.event.listener.PlayerTicker
+import org.wdfeer.infinity_hoe.event.listener.HoldTicker
 import org.wdfeer.infinity_hoe.extension.*
 import org.wdfeer.infinity_hoe.util.TickDurationHelper.secondsToTicks
 import kotlin.random.Random
 
-object MiracleBlessing : HoeEnchantment(Rarity.VERY_RARE), PlayerTicker {
+object MiracleBlessing : HoeEnchantment(Rarity.VERY_RARE), HoldTicker {
     override fun getPath(): String = "miracle_blessing"
 
     override val maxLvl: Int
@@ -22,9 +23,8 @@ object MiracleBlessing : HoeEnchantment(Rarity.VERY_RARE), PlayerTicker {
     override fun getPowerRange(level: Int): IntRange = (17..23).incrementBounds(level * 4)
 
     override fun canIteratePlayers(world: ServerWorld) = world.time % secondsToTicks(7) == 0L
-    override fun tickPlayer(world: ServerWorld, player: ServerPlayerEntity) {
-        val stack = player.inventoryStacks.find { it.hasEnchantment(this) } ?: return
-        val level = stack.getEnchantmentLevel(this)
+    override fun holdTick(world: ServerWorld, player: ServerPlayerEntity, hoe: ItemStack) {
+        val level = hoe.getEnchantmentLevel(this)
 
         effects.take(level + 1).forEach { if (Random.roll(7)) it(player) }
     }
