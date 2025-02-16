e1d3965b81d68fe1bd091d650df2967fa903bbf9
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-02-16 21:50:47 +0100

    Make Fireblast level have an effect

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/very_rare/Fireblast.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/very_rare/Fireblast.kt
@@ -1,6 +1,7 @@
 package org.wdfeer.infinity_hoe.enchantment.unique.very_rare
 
 import net.minecraft.entity.Entity
+import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.projectile.FireballEntity
 import net.minecraft.entity.projectile.ProjectileEntity
 import net.minecraft.item.ItemStack
@@ -9,46 +10,47 @@ import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.Formatting
 import org.wdfeer.infinity_hoe.enchantment.parent.charge.UsableHarvestChargeEnchantment
 import org.wdfeer.infinity_hoe.event.listener.TickListener
-import kotlin.random.Random
+import org.wdfeer.infinity_hoe.extension.getEnchantmentLevel
 
 object Fireblast : UsableHarvestChargeEnchantment(Rarity.VERY_RARE), TickListener {
     override fun useCharge(world: ServerWorld, player: ServerPlayerEntity, hoe: ItemStack): Boolean {
-        world.spawnEntity(createFireball(world, player))
+        world.spawnEntity(createFireball(world, player, hoe.getEnchantmentLevel(this)))
 
         return true
     }
 
-    private var splittingFireballs: MutableList<FireballEntity> = mutableListOf()
+    private var splittingFireballs: MutableList<Pair<FireballEntity, Int>> = mutableListOf()
     private fun createFireball(
         world: ServerWorld,
-        player: ServerPlayerEntity
+        player: ServerPlayerEntity,
+        level: Int
     ): ProjectileEntity {
         val velocity = player.rotationVector.multiply(4.0)
 
-        val fireball = FireballEntity(world, player, velocity.x, velocity.y, velocity.z, 2)
+        val fireball = FireballEntity(world, player, velocity.x, velocity.y, velocity.z, level)
         fireball.setPosition(player.eyePos)
-        splittingFireballs.add(fireball)
+        splittingFireballs.add(fireball to 2 * level + 3)
 
         return fireball
     }
 
     private const val DETONATION_DISTANCE = 8
     override fun postWorldTick(world: ServerWorld) {
-        splittingFireballs.forEach { e -> if (e.owner!!.distanceTo(e) > DETONATION_DISTANCE) detonateFireball(e) }
-        splittingFireballs.removeIf { !it.isAlive }
+        splittingFireballs.forEach { (e, children) -> if (e.owner!!.distanceTo(e) > DETONATION_DISTANCE) detonateFireball(e, children) }
+        splittingFireballs.removeIf { !it.first.isAlive }
     }
 
-    private fun detonateFireball(fireball: FireballEntity) {
+    private fun detonateFireball(fireball: FireballEntity, childCount: Int) {
         val world = fireball.world as? ServerWorld ?: return
-        repeat(3) {
-            val velocity = fireball.velocity.rotateY(Random.nextFloat() - 0.5f)
+        repeat(childCount) {
+            val velocity = fireball.velocity.addRandom(world.random, 2f)
             val entity = FireballEntity(
                 world,
-                fireball.owner as ServerPlayerEntity,
+                fireball.owner as? LivingEntity ?: return,
                 velocity.x,
                 velocity.y,
                 velocity.z,
-                2
+                1
             )
             entity.setPosition(fireball.pos.add(velocity.multiply(1.2)))
             world.spawnEntity(entity)
