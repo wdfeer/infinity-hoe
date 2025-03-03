86b80f3217f22d2c2b8c1e97c293461e5aa9449b
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-03-03 16:02:54 +0100

    Implement healing_mushroom

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/treasure/HealMushroomEnchantment.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/treasure/HealMushroomEnchantment.kt
@@ -1,11 +1,22 @@
 package org.wdfeer.infinity_hoe.enchantment.unique.treasure
 
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.server.world.ServerWorld
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
+import org.wdfeer.infinity_hoe.event.listener.PlayerDamageTaken
+import org.wdfeer.infinity_hoe.event.listener.PlayerTicker
 
-object HealMushroomEnchantment : HoeEnchantment(Rarity.VERY_RARE) {
+object HealMushroomEnchantment : HoeEnchantment(Rarity.VERY_RARE), PlayerDamageTaken, PlayerTicker {
     override fun isTreasure(): Boolean = true
     override fun getPowerRange(level: Int): IntRange = 30..60
     override fun getPath(): String = "healing_mushroom"
 
-    // TODO: Implement
+    override fun preDamageTaken(player: ServerPlayerEntity, amount: Float) {
+        if (amount > 1f) player.heal(0.2f)
+    }
+
+    override fun canIteratePlayers(world: ServerWorld): Boolean = world.time.toInt() % 40 == 0
+    override fun tickPlayer(world: ServerWorld, player: ServerPlayerEntity) {
+        player.heal(0.1f)
+    }
 }
