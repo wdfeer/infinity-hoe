ac31f617217746eaf6867e26303e826f67b9bb74
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-03-03 16:19:47 +0100

    Create shroom lang entries

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/treasure/HealMushroomEnchantment.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/treasure/HealMushroomEnchantment.kt
@@ -5,18 +5,27 @@ import net.minecraft.server.world.ServerWorld
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
 import org.wdfeer.infinity_hoe.event.listener.PlayerDamageTaken
 import org.wdfeer.infinity_hoe.event.listener.PlayerTicker
+import org.wdfeer.infinity_hoe.extension.hasEnchantment
+import org.wdfeer.infinity_hoe.extension.inventoryStacks
 
 object HealMushroomEnchantment : HoeEnchantment(Rarity.VERY_RARE), PlayerDamageTaken, PlayerTicker {
     override fun isTreasure(): Boolean = true
     override fun getPowerRange(level: Int): IntRange = 30..60
     override fun getPath(): String = "healing_mushroom"
 
-    override fun preDamageTaken(player: ServerPlayerEntity, amount: Float) {
-        if (amount > 1f) player.heal(0.2f)
-    }
+    override fun preDamageTaken(player: ServerPlayerEntity, amount: Float) = player.heal(
+        when {
+            player.health < 2f -> 1.5f
+            player.health < 5f -> 1f
+            amount > 1f -> 0.5f
+            else -> 0.1f
+        }
+    )
 
     override fun canIteratePlayers(world: ServerWorld): Boolean = world.time.toInt() % 40 == 0
     override fun tickPlayer(world: ServerWorld, player: ServerPlayerEntity) {
-        player.heal(0.1f)
+        repeat(player.inventoryStacks.count { it.hasEnchantment(this) }) {
+            player.heal(0.1f / (1f + it / 4f))
+        }
     }
 }
--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/treasure/PoisonMushroomEnchantment.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/treasure/PoisonMushroomEnchantment.kt
@@ -10,6 +10,8 @@ import net.minecraft.server.world.ServerWorld
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
 import org.wdfeer.infinity_hoe.event.listener.OnHitListener
 import org.wdfeer.infinity_hoe.event.listener.PlayerTicker
+import org.wdfeer.infinity_hoe.extension.hasEnchantment
+import org.wdfeer.infinity_hoe.extension.inventoryStacks
 
 object PoisonMushroomEnchantment : HoeEnchantment(Rarity.VERY_RARE), OnHitListener, PlayerTicker {
     override fun isTreasure(): Boolean = true
@@ -22,11 +24,13 @@ object PoisonMushroomEnchantment : HoeEnchantment(Rarity.VERY_RARE), OnHitListen
 
     override fun canIteratePlayers(world: ServerWorld): Boolean = world.time.toInt() % 20 == 0
     override fun tickPlayer(world: ServerWorld, player: ServerPlayerEntity) {
-        val target: LivingEntity = world.iterateEntities()
-            .filterIsInstance<LivingEntity>()
-            .filter { it is Monster }
-            .filter { it.distanceTo(player) < 16 }
-            .random()
-        target.addStatusEffect(StatusEffectInstance(StatusEffects.POISON, 2), player)
+        if (player.inventoryStacks.any { it.hasEnchantment(this) }) {
+            val target: LivingEntity = world.iterateEntities()
+                .filterIsInstance<LivingEntity>()
+                .filter { it is Monster }
+                .filter { it.distanceTo(player) < 16 }
+                .random()
+            target.addStatusEffect(StatusEffectInstance(StatusEffects.POISON, 2), player)
+        }
     }
 }
