847a7deb16139512976c44406a402425688713e2
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-03-03 15:54:18 +0100

    Implement FungusEnchanter

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/rare/FungusEnchanter.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/rare/FungusEnchanter.kt
@@ -1,29 +1,93 @@
 package org.wdfeer.infinity_hoe.enchantment.unique.rare
 
+import net.minecraft.block.Blocks
+import net.minecraft.enchantment.EnchantmentHelper
+import net.minecraft.item.ArmorItem
 import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
+import net.minecraft.item.ToolItem
+import net.minecraft.nbt.NbtCompound
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.Formatting
+import net.minecraft.world.biome.BiomeKeys
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
 import org.wdfeer.infinity_hoe.enchantment.parent.charge.ChargeEnchantment
 import org.wdfeer.infinity_hoe.enchantment.unique.treasure.HealMushroomEnchantment
 import org.wdfeer.infinity_hoe.enchantment.unique.treasure.PoisonMushroomEnchantment
 import org.wdfeer.infinity_hoe.event.listener.HoldTicker
+import org.wdfeer.infinity_hoe.extension.hasEnchantment
+import org.wdfeer.infinity_hoe.extension.inventoryStacks
 
 object FungusEnchanter : ChargeEnchantment(Rarity.RARE), HoldTicker {
-    private val mushroomEnchantments: List<HoeEnchantment> by lazy { listOf(
-        PoisonMushroomEnchantment,
-        HealMushroomEnchantment
-    ) }
+    private val mushroomEnchantments: List<HoeEnchantment> by lazy {
+        listOf(
+            PoisonMushroomEnchantment,
+            HealMushroomEnchantment
+        )
+    }
 
     override fun canIteratePlayers(world: ServerWorld): Boolean = world.time % 20L == 0L
 
     override fun holdTick(world: ServerWorld, player: ServerPlayerEntity, hoe: ItemStack) {
-        TODO("Not yet implemented")
+        if (getCharge(hoe) == 0)
+            setCharge(hoe, getMaxCharge(1))
+        else {
+            setCharge(hoe, getCharge(hoe) - getChargingSpeed(world, player))
+            if (getCharge(hoe) <= 0)
+                trigger(player, hoe)
+        }
     }
 
+    private fun getChargingSpeed(world: ServerWorld, player: ServerPlayerEntity): Int {
+        var result = 1
 
-    override fun getMaxCharge(level: Int): Int = 3600
+        val origin = player.blockPos
+
+        if (world.getBiome(origin) == BiomeKeys.MUSHROOM_FIELDS)
+            return 100
+
+        val range = (-6..6)
+        val blockTypes = mutableListOf(
+            Blocks.BROWN_MUSHROOM,
+            Blocks.RED_MUSHROOM,
+            Blocks.POTTED_BROWN_MUSHROOM,
+            Blocks.POTTED_RED_MUSHROOM,
+            Blocks.MUSHROOM_STEM,
+            Blocks.BROWN_MUSHROOM_BLOCK,
+            Blocks.RED_MUSHROOM_BLOCK,
+            Blocks.MYCELIUM
+        )
+
+        range.forEach { x ->
+            range.forEach { y ->
+                range.forEach { z ->
+                    val pos = origin.add(x, y, z)
+                    val block = world.getBlockState(pos).block
+                    if (blockTypes.contains(block)) {
+                        blockTypes.remove(block)
+                        result++
+                    }
+                }
+            }
+        }
+
+        return result
+    }
+
+    private fun trigger(player: ServerPlayerEntity, hoe: ItemStack) {
+        hoe.enchantments.remove(hoe.enchantments.find { EnchantmentHelper.getIdFromNbt(it as NbtCompound?) == getIdentifier() })
+
+        val stack = player.inventoryStacks.filter { it.item != Items.AIR }
+            .filter { it.item is ArmorItem || it.item is ToolItem }
+            .filter { stack -> mushroomEnchantments.any { !stack.hasEnchantment(it) } }
+            .randomOrNull() ?: return
+
+        val enchantment = mushroomEnchantments.filter { !stack.hasEnchantment(it) }.random()
+        stack.addEnchantment(enchantment, 1)
+    }
+
+    override fun getMaxCharge(level: Int): Int = 18000
     override fun getPowerRange(level: Int): IntRange = 0..25
     override fun getPath(): String = "fungus_enchanter"
     override fun getTooltipColor(): Formatting = Formatting.DARK_GREEN
