c399438a59e07358a32727999f41e15b94c36441
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-03-04 07:20:01 +0100

    Make GrowingCapital chance increase with Fortune & Luck

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/uncommon/GrowingCapital.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/uncommon/GrowingCapital.kt
@@ -1,7 +1,9 @@
 package org.wdfeer.infinity_hoe.enchantment.unique.uncommon
 
 import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.enchantment.Enchantments
 import net.minecraft.entity.ItemEntity
+import net.minecraft.entity.effect.StatusEffects
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
@@ -13,7 +15,10 @@ import net.minecraft.util.math.BlockPos
 import org.wdfeer.infinity_hoe.InfinityHoe
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
 import org.wdfeer.infinity_hoe.event.listener.HarvestListener
+import org.wdfeer.infinity_hoe.extension.getEnchantmentLevel
+import org.wdfeer.infinity_hoe.extension.getStatusPotency
 import org.wdfeer.infinity_hoe.extension.roll
+import kotlin.math.max
 import kotlin.random.Random
 
 object GrowingCapital : HoeEnchantment(Rarity.UNCOMMON), HarvestListener {
@@ -34,10 +39,14 @@ object GrowingCapital : HoeEnchantment(Rarity.UNCOMMON), HarvestListener {
     ) {
         if (!FabricLoader.getInstance().isModLoaded(NUMISMATIC_OVERHAUL_ID)) return
 
+        val luck = player.getStatusPotency(StatusEffects.LUCK)
+        val fortune = hoe.getEnchantmentLevel(Enchantments.FORTUNE)
+        val chanceMult = 1f + max(luck / 2f + 0.5f, 0f) + max(fortune / 2f + 0.5f, 0f)
+
         val coinId: String = when {
-            Random.roll(7777) -> "$NUMISMATIC_OVERHAUL_ID:gold_coin"
-            Random.roll(777) -> "$NUMISMATIC_OVERHAUL_ID:silver_coin"
-            Random.roll(77) -> "$NUMISMATIC_OVERHAUL_ID:bronze_coin"
+            Random.roll(1 / 7777f * chanceMult) -> "$NUMISMATIC_OVERHAUL_ID:gold_coin"
+            Random.roll(1 / 777f * chanceMult) -> "$NUMISMATIC_OVERHAUL_ID:silver_coin"
+            Random.roll(1 / 77f * chanceMult) -> "$NUMISMATIC_OVERHAUL_ID:bronze_coin"
             else -> null
         } ?: return
 
