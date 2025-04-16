cbeed9d1f8779655bba27c96ee8c9677433f1e45
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-04-16 15:19:55 +0200

    Implement AlchemyMushroomEnchantment

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/treasure/AlchemyMushroomEnchantment.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/treasure/AlchemyMushroomEnchantment.kt
@@ -1,6 +1,8 @@
 package org.wdfeer.infinity_hoe.enchantment.unique.treasure
 
 import net.minecraft.block.Blocks
+import net.minecraft.block.entity.BlockEntityType
+import net.minecraft.block.entity.BrewingStandBlockEntity
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
@@ -14,17 +16,24 @@ object AlchemyMushroomEnchantment : HoeEnchantment(Rarity.VERY_RARE), PlayerTick
     override fun getPowerRange(level: Int): IntRange = 30..60
     override fun getPath(): String = "alchemy_mushroom"
 
-    override fun canIteratePlayers(world: ServerWorld): Boolean = world.time.toInt() % 25 == 0
+    override fun canIteratePlayers(world: ServerWorld): Boolean = world.time.toInt() % 12 == 0
+
+    private const val TICK_COUNT = 25
     override fun tickPlayer(world: ServerWorld, player: ServerPlayerEntity) {
         if (player.inventoryStacks.none { it.hasEnchantment(this) }) return
 
-        val positions = player.blockPos.getAdjacent(5)
+        val brewingStandPositions = player.blockPos.getAdjacent(5).filter {
+            world.getBlockState(it).block == Blocks.BREWING_STAND
+        }
 
-        for (p in positions) {
-            if (world.getBlockState(p).block == Blocks.BREWING_STAND) {
-                // TODO: tick it
-                break
-            }
+        val pos = brewingStandPositions.random()
+        val state = world.getBlockState(pos)
+
+        val blockEntity = world.getBlockEntity(pos) as? BrewingStandBlockEntity ?: return
+        val ticker = state.getBlockEntityTicker(world, BlockEntityType.BREWING_STAND) ?: return
+
+        repeat(TICK_COUNT) {
+            ticker.tick(world, pos, state, blockEntity)
         }
     }
 }
