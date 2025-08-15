4dcb579f0972436a08f03941c83565c978a649f9
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-08-15 10:33:56 +0200

    Create getPath method (293 errors)

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/HoeEnchantment.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/HoeEnchantment.kt
@@ -2,7 +2,10 @@ package org.wdfeer.infinity_hoe.enchantment
 
 import net.minecraft.enchantment.Enchantment
 import net.minecraft.registry.RegistryKey
+import net.minecraft.util.Identifier
 
 interface HoeEnchantment {
     val registryKey: RegistryKey<Enchantment>
+
+    fun getPath(): Identifier = registryKey.value
 }
