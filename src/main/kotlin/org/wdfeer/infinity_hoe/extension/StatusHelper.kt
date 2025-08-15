0647d1a5e63f81afdf82e3ed75832a5e0e4449ce
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-08-15 10:37:01 +0200

    Fix StatusHelper.kt (275 errors)

--- src/main/kotlin/org/wdfeer/infinity_hoe/extension/StatusHelper.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/extension/StatusHelper.kt
@@ -3,12 +3,13 @@ package org.wdfeer.infinity_hoe.extension
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.effect.StatusEffect
 import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.registry.entry.RegistryEntry
 import kotlin.math.min
 
-fun LivingEntity.getStatusPotency(effect: StatusEffect): Int = this.getStatusEffect(effect)?.amplifier ?: -1
-fun LivingEntity.getStatusDuration(effect: StatusEffect): Int = this.getStatusEffect(effect)?.duration ?: 0
+fun LivingEntity.getStatusPotency(effect: RegistryEntry<StatusEffect?>?): Int = this.getStatusEffect(effect)?.amplifier ?: -1
+fun LivingEntity.getStatusDuration(effect: RegistryEntry<StatusEffect?>?): Int = this.getStatusEffect(effect)?.duration ?: 0
 
-fun LivingEntity.stackStatusPotency(effect: StatusEffect, duration: Int, max: Int, increment: Int = 1) {
+fun LivingEntity.stackStatusPotency(effect: RegistryEntry<StatusEffect?>?, duration: Int, max: Int, increment: Int = 1) {
     val instance = this.getStatusEffect(effect)
     if (instance == null)
         addStatusEffect(StatusEffectInstance(effect, duration, -1 + increment))
@@ -19,7 +20,7 @@ fun LivingEntity.stackStatusPotency(effect: StatusEffect, duration: Int, max: In
     }
 }
 
-fun LivingEntity.stackStatusDuration(effect: StatusEffect, potency: Int, max: Int, increment: Int) {
+fun LivingEntity.stackStatusDuration(effect: RegistryEntry<StatusEffect>, potency: Int, max: Int, increment: Int) {
     val instance = this.getStatusEffect(effect)
     if (instance == null)
         addStatusEffect(StatusEffectInstance(effect, 0 + increment, potency))
