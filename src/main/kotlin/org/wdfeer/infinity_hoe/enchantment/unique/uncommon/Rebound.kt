96aaaca883b5b79b088b78893a867e15db70b0b0
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-08-16 17:46:03 +0200

    Fix rebound and sickle (4E)

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/uncommon/Rebound.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/uncommon/Rebound.kt
@@ -1,11 +1,7 @@
 package org.wdfeer.infinity_hoe.enchantment.unique.uncommon
 
-import net.minecraft.entity.EntityGroup
 import org.wdfeer.infinity_hoe.enchantment.parent.BetterCombatEnchantment
 
 object Rebound : BetterCombatEnchantment() {
     override fun getPath(): String = "rebound"
-
-    // FIXME: define the attribute in json
-    override fun getAttackDamage(level: Int, group: EntityGroup?): Float = -0.5f
 }
--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/uncommon/Sickle.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/uncommon/Sickle.kt
@@ -1,11 +1,7 @@
 package org.wdfeer.infinity_hoe.enchantment.unique.uncommon
 
-import net.minecraft.entity.EntityGroup
 import org.wdfeer.infinity_hoe.enchantment.parent.BetterCombatEnchantment
 
 object Sickle : BetterCombatEnchantment() {
     override fun getPath(): String = "sickle"
-
-    // FIXME: define the attribute in json
-    override fun getAttackDamage(level: Int, group: EntityGroup?): Float { return 0.5f }
 }
