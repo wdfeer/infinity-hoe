1b9f6b4ccd7ebd6191acbf3cda24536863abaec8
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-08-16 17:37:26 +0200

    Fix SpeedMushroomEnchantment (10E)

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/treasure/SpeedMushroomEnchantment.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/treasure/SpeedMushroomEnchantment.kt
@@ -5,6 +5,8 @@ import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.item.Items
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.Identifier
+import org.wdfeer.infinity_hoe.InfinityHoe.MOD_ID
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
 import org.wdfeer.infinity_hoe.event.listener.PlayerTicker
 import org.wdfeer.infinity_hoe.extension.addTemporary
@@ -19,17 +21,17 @@ object SpeedMushroomEnchantment : HoeEnchantment, PlayerTicker {
     override fun canIteratePlayers(world: ServerWorld): Boolean = world.time % 21 == 0L
 
     override fun tickPlayer(world: ServerWorld, player: ServerPlayerEntity) {
-        val uuid = UUID.nameUUIDFromBytes(getPath().toByteArray())
-        val modifier = player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED)?.getModifier(uuid)
+        val id = Identifier.of(MOD_ID, getPath())
+        val modifier = player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED)?.getModifier(id)
 
         if (player.inventoryStacks.any { it.item != Items.AIR && it.hasEnchantment(this) }) {
             if (modifier == null)
                 player.attributes.addTemporary(
-                    EntityAttributes.GENERIC_MOVEMENT_SPEED,
-                    EntityAttributeModifier(uuid, getPath(), 0.015, EntityAttributeModifier.Operation.ADDITION)
+                    EntityAttributes.GENERIC_MOVEMENT_SPEED.value(),
+                    EntityAttributeModifier(id, 0.015, EntityAttributeModifier.Operation.ADD_VALUE)
                 )
         } else if (modifier != null) {
-            player.attributes.remove(EntityAttributes.GENERIC_MOVEMENT_SPEED, modifier)
+            player.attributes.remove(EntityAttributes.GENERIC_MOVEMENT_SPEED.value(), modifier)
         }
     }
 }
