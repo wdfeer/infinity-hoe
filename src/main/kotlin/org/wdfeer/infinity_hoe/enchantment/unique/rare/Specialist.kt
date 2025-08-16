ce43b462e5bad75c06ed39e4a397193df07308df
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-08-16 11:01:19 +0200

    Define Specialist tag and exclusive set

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/rare/Specialist.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/rare/Specialist.kt
@@ -18,8 +18,6 @@ class Specialist(private val path: String, private val crop: Block) : HoeEnchant
     DoubleHarvestEnchantment, HarvestListener {
     override fun getPath(): String = path
 
-    override fun canAccept(other: Enchantment?): Boolean = other !is Specialist
-
     override fun onCropBroken(
         world: ServerWorld,
         player: ServerPlayerEntity,
