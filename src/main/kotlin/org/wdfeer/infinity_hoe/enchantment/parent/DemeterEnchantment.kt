afc76367f3420426cf59f03a2cb9e50500a90449
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-08-16 15:07:53 +0200

    Implement demeter mutual incompatibility (72E)

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/parent/DemeterEnchantment.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/parent/DemeterEnchantment.kt
@@ -1,11 +1,8 @@
 package org.wdfeer.infinity_hoe.enchantment.parent
 
-import net.minecraft.enchantment.Enchantment
 import net.minecraft.util.Formatting
 import org.wdfeer.infinity_hoe.enchantment.parent.charge.HarvestChargeEnchantment
 
 abstract class DemeterEnchantment : HarvestChargeEnchantment() {
     override fun getTooltipColor(): Formatting = Formatting.GREEN
-
-    override fun canAccept(other: Enchantment?): Boolean = other !is DemeterEnchantment
 }
