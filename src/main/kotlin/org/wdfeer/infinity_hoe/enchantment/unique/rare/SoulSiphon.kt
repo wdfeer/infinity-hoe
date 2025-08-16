f0a8e094680b2e21e682efb4f2c5ab7eaee16d6f
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-08-16 17:34:45 +0200

    Fix SoulSiphon (18E)

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/rare/SoulSiphon.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/unique/rare/SoulSiphon.kt
@@ -6,6 +6,8 @@ import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.damage.DamageTypes
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.Identifier
+import org.wdfeer.infinity_hoe.InfinityHoe.MOD_ID
 import org.wdfeer.infinity_hoe.enchantment.HoeEnchantment
 import org.wdfeer.infinity_hoe.enchantment.parent.CropCatalyzer
 import org.wdfeer.infinity_hoe.event.listener.PlayerTicker
@@ -22,31 +24,30 @@ object SoulSiphon : HoeEnchantment, PlayerTicker, CropCatalyzer {
 
     override fun tickPlayer(world: ServerWorld, player: ServerPlayerEntity) {
         val attribute = EntityAttributes.GENERIC_MAX_HEALTH
-        val mod = player.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH)?.getModifier(modifierUUID)
+        val mod = player.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH)?.getModifier(modifierId)
 
         val hoe = player.handItems.find { !it.isEmpty && it.hasEnchantment(this) }
 
         if (hoe == null || player.health <= 2) {
             if (mod != null && Random.roll(3)) {
-                recover(mod, player, attribute)
+                recover(mod, player, attribute.value())
             }
 
             return
         }
 
         if (catalyze(world, player, 3, hoe, 0.5f)) {
-            player.attributes.addTemporary(attribute, getModifier(mod?.value?.minus(2) ?: -2.0))
+            player.attributes.addTemporary(attribute.value(), getModifier(mod?.value?.minus(2) ?: -2.0))
 
             player.damage(DamageTypes.MAGIC, 2f)
         }
     }
 
-    private val modifierUUID = UUID.nameUUIDFromBytes("soul_siphon".toByteArray())
+    private val modifierId = Identifier.of(MOD_ID,"soul_siphon")
     private fun getModifier(hpIncrease: Double) = EntityAttributeModifier(
-        modifierUUID,
-        "soul_siphon",
+        modifierId,
         hpIncrease,
-        EntityAttributeModifier.Operation.ADDITION
+        EntityAttributeModifier.Operation.ADD_VALUE
     )
 
     private fun recover(
