aa3fdb50241e4eb5ca3c5fbb148e229bbe5afe30
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-08-16 07:36:03 +0200

    Recreate HoeEnchantment interface

--- src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/HoeEnchantment.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/enchantment/HoeEnchantment.kt
@@ -2,10 +2,11 @@ package org.wdfeer.infinity_hoe.enchantment
 
 import net.minecraft.enchantment.Enchantment
 import net.minecraft.registry.RegistryKey
+import net.minecraft.registry.RegistryKeys
 import net.minecraft.util.Identifier
+import org.wdfeer.infinity_hoe.InfinityHoe.MOD_ID
 
 interface HoeEnchantment {
-    val registryKey: RegistryKey<Enchantment>
-
-    fun getPath(): Identifier = registryKey.value
+    val registryKey: RegistryKey<Enchantment> get() = RegistryKey.of(RegistryKeys.ENCHANTMENT, Identifier.of(MOD_ID, getPath()))
+    fun getPath(): String
 }
