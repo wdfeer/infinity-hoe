11078bc2adcfa06d09f3f459ae008a0e8a6a3939
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-03-05 11:50:23 +0100

    Fix ServerPlayerEntityMixin

--- src/main/java/org/wdfeer/infinity_hoe/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/org/wdfeer/infinity_hoe/mixin/ServerPlayerEntityMixin.java
@@ -2,6 +2,7 @@ package org.wdfeer.infinity_hoe.mixin;
 
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,12 +12,12 @@ import org.wdfeer.infinity_hoe.event.emitter.PlayerDamageTaken;
 @Mixin(ServerPlayerEntity.class)
 abstract class ServerPlayerEntityMixin {
     @Inject(method = "damage", at = @At("HEAD"))
-    void injectPreDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+    void injectPreDamage(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         PlayerDamageTaken.INSTANCE.mixinPreDamageTaken((ServerPlayerEntity) (Object) this, amount);
     }
 
     @Inject(method = "damage", at = @At("RETURN"))
-    void injectPostDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+    void injectPostDamage(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         PlayerDamageTaken.INSTANCE.mixinPostDamageTaken((ServerPlayerEntity) (Object) this, amount);
     }
 }
