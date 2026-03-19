136d1d9f0d75492074a41e89486d2979d63aaca4
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-03-19 15:44:40 +0100

    Use custom getEntry for damage type registry keys

--- src/main/kotlin/org/wdfeer/infinity_hoe/extension/RegistryKey.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/extension/RegistryKey.kt
@@ -1,10 +1,14 @@
 package org.wdfeer.infinity_hoe.extension
 
+import net.minecraft.entity.damage.DamageType
 import net.minecraft.registry.Registry
 import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.Registries
+import net.minecraft.registry.RegistryKeys
 import net.minecraft.registry.entry.RegistryEntry
 import net.minecraft.registry.entry.RegistryEntryOwner
+import net.minecraft.server.world.ServerWorld
+import net.minecraft.world.World
 
 @Suppress("UNCHECKED_CAST")
 fun <T> RegistryKey<T>.getValue(): T? {
@@ -15,10 +19,15 @@ fun <T> RegistryKey<T>.getValue(): T? {
 
 @Suppress("UNCHECKED_CAST")
 fun <T> RegistryKey<T>.getEntry(): RegistryEntry<T>? {
+    assert(this.registry.path != "damage_type") { "Use getEntry(World) for damage types!" }
+
     val registry = Registries.REGISTRIES[this.registry] as? Registry<T>
         ?: return null
     return registry.getEntry(this.value).orElse(null)
 }
 
+fun RegistryKey<DamageType>.getEntry(world: World): RegistryEntry<DamageType> =
+    world.registryManager.getEntryOrThrow(RegistryKeys.DAMAGE_TYPE).value().getEntry(this.value).get()
+
 fun <T> RegistryKey<T>.getPlaceholderEntry(): RegistryEntry<T> =
-    RegistryEntry.Reference.standAlone<T>(object : RegistryEntryOwner<T> { }, this)
+    RegistryEntry.Reference.standAlone<T>(object : RegistryEntryOwner<T> {}, this)
