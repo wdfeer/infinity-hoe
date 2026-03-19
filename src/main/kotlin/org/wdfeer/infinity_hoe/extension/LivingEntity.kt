3fa4a9e84e47764a5db6dda8f4febfa77327ae3c
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-03-19 15:50:37 +0100

    Actually fix LivingEntity.damage

--- src/main/kotlin/org/wdfeer/infinity_hoe/extension/LivingEntity.kt
+++ src/main/kotlin/org/wdfeer/infinity_hoe/extension/LivingEntity.kt
@@ -8,6 +8,7 @@ import net.minecraft.server.world.ServerWorld
 
 fun LivingEntity.damage(damageType: RegistryKey<DamageType>, amount: Float, attacker: LivingEntity) {
     val world = this.world as? ServerWorld ?: return
-    val damageSource = DamageSource(damageType.getEntry(attacker.world), attacker)
+    val entry = damageType.getEntry(attacker.world)
+    val damageSource = DamageSource(entry, attacker)
     this.damage(world, damageSource, amount)
 }
