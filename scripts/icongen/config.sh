2578296979f9a428ceeaae7803757413d9315485
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-06-27 08:05:26 +0200

    Change icongen text color to purple

--- scripts/icongen/config.sh
+++ scripts/icongen/config.sh
@@ -1,5 +1,5 @@
 export BASE_IMAGE="src/main/resources/no_include/icon_base.png"
 export OUTPUT_PNG="src/main/resources/assets/infinity_hoe/icon.png"
 
-export FONT_COLOR="white"
+export FONT_COLOR="purple"
 export FONT="Inter"
